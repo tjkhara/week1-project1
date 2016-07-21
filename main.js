b1732f317d071b34eb08a6f754cddd0500baa533
tjkhara <tkhara@gmail.com>
2016-07-22 03:07:53 +0530

    Update main.js
    Simple track problems finished.

--- main.js
+++ main.js
@@ -25,12 +25,15 @@ ADVANCED TRACK: 8-11
 
 // 1. Log your name to the console (as a string).
 console.log("Question 1");
+console.log("TJ Khara");
 
 
 
 
 // 2. Declare a new variable called 'name',
 //    and then log it to the console.
+var name;
+console.log(name);
 
 
 
@@ -39,7 +42,13 @@ console.log("Question 1");
 // 3. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
 
+var numone = 1;
+var numtwo = 2;
 
+numone + numtwo;
+numone - numtwo;
+numone * numtwo;
+numone / numtwo;
 
 
 
@@ -47,27 +56,37 @@ console.log("Question 1");
 //    Create a third variable that concatenates the two strings
 //    that also places a space between the strings.
 
-
+var stringone = "hello", stringtwo = "world";
+var totalstring = stringone + stringtwo;
+var totalstring = stringone + " " + stringtwo;
+console.log(totalstring);
 
 
 
 // 5. Add, subract, multiply and divide the strings '5' and '3'.
 //    Observe the results and brieftly explain the results within the console.
 
-
+'5' + '3' = 53 // seems like simple concatenation
+'5' - '3' = 2 // seems like JS is first converting these into numbers and them performing the subtraction
+'5' * '3' = 15 // converting to numbers
+'5' / '3' = 1.666667 // converting to numbers
 
 
 
 // 6. Create an simple alert stating anything you wish. 
 
-
+alert ("hello");
 
 
 
 // 7. Create a prompt that asks a user to pick a number.
 //    Then double user's answer.
 
+prompt ("Pick a number");
 
+var numberpicked = prompt ("Pick a number");
+
+var doublenumber = 2 * numberpicked;
 
 
 // ADVANCED TRACK
